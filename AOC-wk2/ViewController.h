c1b165e5d869035cceff08e12170ef1511ee2154
John plank <johnplank211@gmail.com>
2013-02-14 14:33:45 -0500

    Initial Commit

---
+++ AOC-wk2/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AOC-wk2
+//
+//  Created by john plank on 2/14/13.
+//  Copyright (c) 2013 john plank. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
